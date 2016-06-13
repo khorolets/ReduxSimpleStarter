8ae68631976c03eb5ac52c26c29a2d3c329069db
Bohdan Khorolets <bogdan@khorolets.com>
2016-06-13 11:01:37 +0300

    bundle css and command for it

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import '../style/style.css';
 
 import App from './components/app';
 import reducers from './reducers';
